880070261e78245f60f248f63d58b504c46866b4
alem7486 <108939088+alem7486@users.noreply.github.com>
2022-08-22 19:50:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,73 +1,101 @@
-//Hoteles
+const divDetails = document.getElementsByClassName('hotel-details')
 
 class Hotel {
+
     constructor(id, nombre, direccion, habitaciones) {
-        this.id = id
-        this.nombre = nombre
-        this.direccion = direccion
-        this.habitaciones = habitaciones
+
+        _this_.id = id
+
+        _this_.nombre = nombre
+
+        _this_.direccion = direccion
+
+        _this_.habitaciones = habitaciones
+
     }
+
 }
+
 const testhotel = [
-    // disponibilidad
+
+    _// disponibilidad_
+
     { single: 3, precio: 10000 }, { doble: 1 }, { triple: 5 }
+
 ]
 
 const hotel1 = new Hotel(1, "Mar del Plata", "Tucumán 2375", testhotel, true)
-const hotel2 = new Hotel(2, "Miramar", "Av. San Martín 1489", testhotel) // false
+
+const hotel2 = new Hotel(2, "Miramar", "Av. San Martín 1489", testhotel) _// false_
+
 const hotel3 = new Hotel(3, "Necochea", "Centenario 144", testhotel, true)
-const hotel4 = new Hotel(4, "San Bernardo", "La costa 3100", testhotel) // false
+
+const hotel4 = new Hotel(4, "San Bernardo", "La costa 3100", testhotel) _// false_
 
 const hoteles = [hotel1, hotel2, hotel3, hotel4]
 
-//Habitaciones
+_[//habitaciones_](//habitaciones_%60%60%60)
 
 class Habitacion {
+
     constructor(id, nombre) {
-        this.id = id
-        this.nombre = nombre
+
+        _this_.id = id
+
+        _this_.nombre = nombre
+
     }
+
 }
 
 const habitacion1 = new Habitacion(1, "Single")
+
 const habitacion2 = new Habitacion(2, "Doble")
+
 const habitacion3 = new Habitacion(3, "Triple")
+
 const habitacion4 = new Habitacion(4, "Múltiple")
 
 const habitaciones = [habitacion1, habitacion2, habitacion3, habitacion4]
 
-// Input date validation
+_// Input date validation_
+
 const dateControl = document.getElementById('inputDate1')
+
 const hoy = new Date()
-const dateMin = `${hoy.getFullYear()}-0${hoy.getMonth() + 1}-${hoy.getDate()}`
+
+```const dateMin = `${hoy.getFullYear()}-0${hoy.getMonth() + 1}-${hoy.getDate()}````
+
 dateControl.setAttribute('min', dateMin)
 
+_//Validación de la búsqueda_
 
-//Validación de la búsqueda
 const idForm = document.getElementById('button-search')
 
-idForm.addEventListener('click', (evt) => {
-    const divDetails = document.getElementsByClassName('hotel-details')
-    
+idForm.addEventListener('submit', (evt) => {
 
-    // console.log("Hiciste clic", evt)
+evt.preventDefault()
+
+    _// console.log("Hiciste clic", evt)_
+
     const select = document.getElementById('seleccionHotel')
+
     const hotelEncontrado = hoteles.find((element) => element.id == select.value);
+
     console.log(hotelEncontrado)
 
     let p = document.createElement("p")
 
-
     divDetails[0].append(`Dirección: ${hotelEncontrado.direccion}`, p)
-    divDetails[0].append(`Habitaciones: ${JSON.stringify(hotelEncontrado.habitaciones)}`, p)
-    // divDetails[0].append("Some text", p)
-    // divDetails[0].append("Some text", p)
-    // divDetails[0].append("Some text", p)
-})
 
+    divDetails[0].append(`Habitaciones: ${JSON.stringify(hotelEncontrado.habitaciones)}`, p)
+
+})
 
 const inputText = document.getElementById('inputText')
 
 inputText.addEventListener("change", () => {
+
     console.log(inputText.value)
+
 })
