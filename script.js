4571a3df1bf12c157bbd1991e58a7e227d313c61
alem7486 <108939088+alem7486@users.noreply.github.com>
2022-08-26 17:09:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,40 @@
 const divDetails = document.getElementById('hotelDetails')
 const buttonSearch = document.getElementById('buttonSearch')
 
-class Hotel {
-    constructor(id, nombre, direccion, habitaciones) {
-        this.id = id
-        this.nombre = nombre
-        this.direccion = direccion
-        this.habitaciones = habitaciones
+const MarDelPlata = {id: 1, direccion: 'Peralta Ramos 2500',
+    habitacion: {
+        single: {precio: 2000, cantidad: 25},
+        doble: {precio: 3000, cantidad: 30},
+        triple: {precio: 4000, cantidad: 20},
+        multiple: {precio: 5000, cantidad: 15}
     }
 }
 
-const hotel1 = new Hotel(1, "Mar del Plata", "Tucumán 2375")
-const hotel2 = new Hotel(2, "Miramar", "Av. San Martín 1489",)
-const hotel3 = new Hotel(3, "Necochea", "Centenario 144")
-// const hotel4 = new Hotel(4, "San Bernardo", "La costa 3100", testhotel) // false
-
-// const hoteles = [hotel1, hotel2, hotel3, hotel4]
-const hoteles = [hotel1, hotel2, hotel3]
-
-
-//Habitaciones
-class Habitacion {
-    constructor(id, nombre) {
-        this.id = id
-        this.nombre = nombre
+const Miramar = {id: 2, direccion: 'Av. 9 1174',
+    habitacion:{
+        single: {precio: 1500, cantidad: 20},
+        doble: {precio: 2000, cantidad: 15},
+        triple: {precio: 2500, cantidad: 10}
     }
 }
 
-const habitacion1 = new Habitacion(1, "Single")
-const habitacion2 = new Habitacion(2, "Doble")
-const habitacion3 = new Habitacion(3, "Triple")
-const habitacion4 = new Habitacion(4, "Múltiple")
+const Necochea = {id: 3, direccion: 'Centenario 215',
+    habitacion:{
+        single: {precio: 1800, cantidad: 16},
+        doble: {precio: 2100, cantidad: 15},
+        triple: {precio: 2700, cantidad: 10}
+    }
+}
+const SanBernardo = {id: 4, direccion: 'La costa 901',
+    habitacion:{
+        single: {precio: 1600, cantidad: 9},
+        doble: {precio: 2000, cantidad: 7},
+        triple: {precio: 2200, cantidad: 5}
+    }
+}
 
-const habitaciones = [habitacion1, habitacion2, habitacion3, habitacion4]
+const hoteles = [MarDelPlata, Miramar, Necochea, SanBernardo]
+
 
 // Input date validation
 const dateControl = document.getElementById('inputDate1')
@@ -45,32 +47,35 @@ dateControl.setAttribute('min', dateMin)
 const idForm = document.getElementById('idForm')
 
 idForm.addEventListener('submit', (evt) => {
-evt.preventDefault()
-
-    // console.log("Hiciste clic", evt)
-    const select = document.getElementById('seleccionHotel')
+    evt.preventDefault()
+    
+    //mostrar seleccion
+    const select = document.getElementById('seleccionHotel', 'habitacionElegida')
     const hotelEncontrado = hoteles.find((element) => element.id == select.value);
     console.log(hotelEncontrado)
-
+    
     let p = document.createElement("p")
-
-
+    
+    
     divDetails.innerHTML=`
-    Direrccion: ${hotelEncontrado.direccion}`
+    Dirección: ${hotelEncontrado.direccion}, 
+    Habitación: ${hotelEncontrado.habitacion}`
 
-    // divDetails[0].append(`Dirección: ${hotelEncontrado.direccion}`, p)
-    // divDetails[0].append(`Habitaciones: ${JSON.stringify(hotelEncontrado.habitaciones)}`, p)
+    
+})
 
+localStorage.setItem('eleccion', JSON.stringify(hoteles))
+
+buttonSearch.addEventListener('click', () => {
+    const eleccionStorage = JSON.parse(localStorage.getItem('eleccion'))
 })
 
 buttonSearch.addEventListener('click',()=>{
     divDetails.innerHTML=''
 })
 
-
 const inputText = document.getElementById('inputText')
 
 inputText.addEventListener("change", () => {
     console.log(inputText.value)
 })
-
